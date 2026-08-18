790cf911d162d049236461a609b9f16f6ffb9d80
Mariano Santoro <mariano_santoro@shilab.com>
2026-08-18 14:51:09 -0500

    chore: add missing newline in MaxRetryAttemptsReachedError constructor

--- src/utilities/public/errors/maxRetryAttemptsReachedError.ts
+++ src/utilities/public/errors/maxRetryAttemptsReachedError.ts
@@ -6,6 +6,7 @@ import { ScopedError } from './scopedError.js';
 export class MaxRetryAttemptsReachedError extends ScopedError {
     constructor(public readonly attempts: number, public readonly lastError: unknown, public readonly context?: string) {
         const error = lastError instanceof Error ? lastError.message : String(lastError);
+
         const retryAttempts = Math.max(0, attempts - 1);
 
         super(`Max number of retry attempts reached (${ retryAttempts }) after ${ attempts } total attempts. Last error: ${ error }`, context);
